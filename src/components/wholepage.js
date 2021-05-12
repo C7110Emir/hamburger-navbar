eebdd7be180c8d513170ec14107d9c82bb93676d
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-12 11:23:44 -0400

    edit

--- src/components/wholepage.js
+++ src/components/wholepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import image from "./cd2246d5d0f54f9a4316bd4d276764b2.png"
 import {useEffect,useState,useLayoutEffect} from "react"
 import "./wholepage.css"
+import dot from "./9dot.PNG"
 
 
 function Wholepage() {
@@ -11,19 +12,25 @@ function Wholepage() {
     setInterval(() =>{
         setWindowwidth(window.innerWidth)
     },1000)
+    const handleClick = () =>{
+        setDropdown(!dropdown)
+    }
     return (
         <div style={{width:"100vw",height:"99vh"}} >
         <div className="outerDiv" >
             <div className="imagediv">
                 <img src={image} alt="logo" className="image"/>
             
-           {532 <  windowwith ? 
+           {700 <  windowwith ? 
            <div className="biggerversionouterdiv">
                <a>Works</a>
                <a>About</a>
                <a>Career</a>
                <a>Contact</a>
-           </div>:<div></div>
+           </div>:<div className="smallerversionouterdiv">
+                <img src={dot} alt="button" onClick={handleClick} className="button"/>
+                
+           </div>
             }
             </div>
         </div>
